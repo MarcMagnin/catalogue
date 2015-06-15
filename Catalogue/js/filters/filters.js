65f38ed3ad2d0d835a53542a0f798b0d3ea7b981
Marc <marc.magnin@gmail.com>
2015-06-15 14:56:47 +0200

    up

--- Catalogue/js/filters/filters.js
+++ Catalogue/js/filters/filters.js
@@ -1,11 +1,19 @@
 ﻿
-app.filter('suggestionfilter', function () {
-    return function (array, value) {
-        console.log(array['$$state'])
-        var selected = new array();
-        if ($.grep(array, function (e) { return e == value; }).length == 0) {
-            selected.push(e)
-        }
-        return selected;
-    };
-});
+//app.filter('suggestionfilter', function () {
+//    return function (inputArray, value) {
+//        console.log(inputArray)
+//        console.log(value)
+//        //var outputArray = [];
+//        //if ($.grep(array, function (e) { return e == value; }).length == 0) {
+//        //    selected.push(e)
+//        //}
+//        angular.forEach(inputArray, function (item) {
+//            console.log(item)
+//            if (item.indexOf(value.viewValue) !== -1) {
+//                outputArray.push(item);
+//            }
+//        });
+
+//        return outputArray;
+//    };
+//});
