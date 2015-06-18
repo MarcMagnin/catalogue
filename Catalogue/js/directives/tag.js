c896ae3367b772fe858e80344327695b0a4ba585
Marc <marc.magnin@gmail.com>
2015-06-18 09:15:23 +0200

    up

--- Catalogue/js/directives/tag.js
+++ Catalogue/js/directives/tag.js
@@ -1,5 +1,5 @@
 ﻿
-app.directive('tag', function ($http, $rootScope) {
+app.directive('tag', function ($http, $rootScope, $timeout) {
     return {
         restrict: 'E',
         scope: {
@@ -15,26 +15,30 @@ app.directive('tag', function ($http, $rootScope) {
         '{{tag}}' +
         '<md-icon class="md-icon tag-remove-icon" md-svg-src="images/close.svg"></md-icon>' +
 '</button>' +
-            '</div>' +
-             '<p><input type="text" ' +
+            '</div><form name="myForm" ng-submit="myForm.$valid && submit()">' +
+             '<p><input name="myInput" type="text" ' +
                 'ng-model="new_value"' +
                 'typeahead="tags.Val for tags in getData($viewValue) | filter:$viewValue" ' +
                 'typeahead-loading="loading" ' +
                 'typeahead-focus-first="false" ' +
-                'typeahead-on-select="onSelect($item, $model, $label)"' +
-                'class="input"></input></p>'+
+                'typeahead-editable="true" ' +
+                'typeahead-on-select="onSelect($item, $model, $label); asyncSelected = \'\'" ' +
+                'class="input"></input></form></p>' +
             '</div>',
         //'<i ng-show="loading" class="glyphicon glyphicon-refresh"></i> ' +
         //'<a class="btn" ng-click="add()">Ajouter</a>'
 
-        link: function ($scope, $element) {
+        link: function ($scope, $element, $attrs, $modelCtrl) {
             // FIXME: this is lazy and error-prone
             //var input = angular.element($element.children()[1]);
             var input = $($element).first("input");
+            var test = input.controller().$viewValue;
             // This adds the new tag to the tags array
             $scope.add = function () {
                 if (!$scope.new_value)
                     return;
+
+             
 
                 if (!$scope.item[$scope.fieldname])
                     $scope.item[$scope.fieldname] = new Array();
@@ -100,15 +104,37 @@ app.directive('tag', function ($http, $rootScope) {
 
                     });
             };
-
-            // Capture all keypresses
-            input.bind('keypress', function (event) {
+            $scope.submit = function () {
+                $scope.add();
+                var instance = $scope.myForm.myInput;
+                instance.$setViewValue('');
+                instance.$render();
+            }
+            //input.bind('keypress', function (event) {
+            //    // But we only care when Enter was pressed
+            //    if (event.keyCode == 13) {
+            //        // There's probably a better way to handle this...
+            //        $scope.add();
+            //    }
+            
+            //});
+            input.bind('keyup', function (event) {
                 // But we only care when Enter was pressed
                 if (event.keyCode == 13) {
                     // There's probably a better way to handle this...
-                    $scope.add();
+                    event.preventDefault();
+                    //setTimeout(function(){
+                    //    $('.input').blur();
+                    //},1000)
+                    
+                    $scope.$apply();
                 }
+
             });
+            $scope.$watch('new_value', function (newValue) {
+                if (newValue == '')
+                    delete $scope.new_value;
+            })
         }
     };
 });
